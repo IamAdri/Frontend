6061535afe1e39f9e278138c9305ea39ea9663d2
IamAdri <atoma304@gmail.com>
2026-03-23 21:43:44 +0200

    Tested posting new customer

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import React from "react";
 
 const rows: GridRowsProp = [
   {
@@ -27,11 +28,39 @@ const columns: GridColDef[] = [
 ];
 
 export default function RenderComponent() {
+  const handleCreateCustomer = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3001/customers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        companyName: "SunSRL",
+        contactName: "Mirela Ceban",
+        contactEmail: "sun@gmail.com",
+        industry: "e-commerce",
+        projectType: "web_platform",
+        status: "scheduled",
+        deadline: "2026-03-23T10:30:00Z",
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div className="m-15">
       <div style={{ height: 300, width: "100%" }}>
         <DataGrid rows={rows} columns={columns} />
       </div>
+      <button
+        className="mt-15 border-solid border-1 cursor-pointer"
+        onClick={handleCreateCustomer}
+      >
+        Post new customer
+      </button>
     </div>
   );
 }
